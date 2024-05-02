77af3bf0401e3f06182bfdd19a31cb40440a3112
SimoneGiusso <sgiusso@edgelab.ch>
2024-05-02 17:30:27 +0200

    Add readme and improve comments

--- Physical Replication/demo.sql
+++ Physical Replication/demo.sql
@@ -1,26 +1,26 @@
--- 1. CHECKING IF SETUP IS WORKING
+-- 1. MAKING SURE EVERYTHING'S UP AND RUNNING
 
--- Primary Server should have a walsender process
+-- Primary Server: Checking for the presence of a walsender process
 SELECT *
-FROM pg_catalog.pg_stat_activity 
+FROM pg_catalog.pg_stat_activity
 WHERE backend_type = 'walsender'
 
--- Standby Server should have a walreceiver process
+-- Standby Server: Verifying the existence of a walreceiver process
 SELECT *
-FROM pg_catalog.pg_stat_activity 
+FROM pg_catalog.pg_stat_activity
 WHERE backend_type = 'walreceiver'
 
--- Finally the standby server should contain the same data of the primary server
+-- Ensuring the standby server mirrors the primary server's data
 SELECT *
 FROM authors
 
--- 2. STREAMING REPLICATION IN ACTION
+-- 2. WATCHING REPLICATION IN ACTION
 
--- movies table doesn't exist on standby server
+-- Standby Server: Confirming absence of the 'movies' table
 SELECT *
 FROM movies
 
--- Create movies table with foreign key on primary server and insert some rows
+-- Setting up 'movies' table with foreign key on the primary server and inserting data
 CREATE TABLE movies (
     movie_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -35,38 +35,40 @@ INSERT INTO movies (title, release_year, director, genre, author_id) VALUES
     ('The Godfather', 1972, 'Francis Ford Coppola', 'Crime', 2),
     ('The Dark Knight', 2008, 'Christopher Nolan', 'Action', 3);
 
--- Data should have been replicated on the standby server
+-- Verifying data replication on the standby server
 SELECT *
 FROM movies
 
--- 3. READONLY STANDBY AND WRITING QUERIES
+-- 3. STANDBY FOR READS, BUT WRITES?
 
--- Although only read queries can be performed on standby, write queries can still run on the standby as result of replication
--- Open a transaction in standby server
+-- Although standby allows only read queries, writes can still occur due to replication
+-- Initiating a transaction on the standby server
 BEGIN;
 SELECT * FROM movies;
 
--- Add new column on movies table in primary server
+-- Adding a new column to the 'movies' table on the primary server
 ALTER TABLE movies ADD COLUMN rating int;
 
--- Open a new connection to the standby. The following query won't run because the ALTER TABLE statement queued, which has priority, is blocked by the opened transaction.
+-- Attempting a query on the standby server. However, it's blocked due to the queued ALTER TABLE statement.
 SELECT * FROM movies;
 
--- Unblock the recently opened connection by running END; on standby
--- If you don't do it a mechanism provided by postgresql, and active by default, will forcibly cancel the standby query that conflict with to-be-applied WAL records. The delay, before the query get cancelled, can be controlled via the max_standby_streaming_delay parameter (30s by default).
--- This error will be then showed to the client in the stanby:
+-- Resuming the blocked connection on standby by running END;
+-- Without this, PostgreSQL's default mechanism forcibly cancels conflicting standby queries with to-be-applied WAL records.
+-- The client on standby receives an error:
 
 -- SQL Error [40001]: FATAL: terminating connection due to conflict with recovery
---  Detail: User was holding a relation lock for too long.
---  Hint: In a moment you should be able to reconnect to the database and repeat your command.
+-- Detail: User was holding a relation lock for too long.
+-- Hint: You should be able to reconnect soon and retry your command.
 
--- The problem doesn't happen if the opened transaction is on the primary, since the statement are sent after commit
+-- Total conflicts can be monitored with SELECT * FROM pg_stat_database_conflicts on standby
 
--- Primary server connection 1
+-- No such issue occurs if the transaction is initiated on the primary server, as statements are sent post-commit.
+
+-- Primary Server Connection 1
 BEGIN;
 SELECT * FROM movies;
 
--- Primary server connection 2
+-- Primary Server Connection 2
 ALTER TABLE movies ADD COLUMN budget float;
 
--- At this point SELECT * FROM movies; on primary server will wait. However it will run on the standby.
+-- At this point, SELECT * FROM movies; on primary server will wait, while it executes on standby.
